45d67ef457f939bc67289afb03145aecfa9c0e75
Przemysław Dąbek <przemyslaw.dabek@uxpin.com>
2016-09-13 00:31:46 +0200

    :pl as a default locale

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Oceny
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :pl
     config.assets.paths << Rails.root.join('app/assets/fonts')
     config.assets.precompile += %w( form.css )
     config.active_job.queue_adapter = :delayed_job
